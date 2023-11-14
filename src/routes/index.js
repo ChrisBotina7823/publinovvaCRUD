bec8c8877679390c1e0010acf1180c7e9429a975
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2023-11-14 10:47:03 -0500

    Fix mysql logging in problem when trying binlogs

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,6 @@ const router = express.Router();
 
 router.get('/', isNotAuthenticated, async (req, res) => {
     console.log(req.isAuthenticated())
-    const ans = await pool.query("mysqlbinlog 'binlog.000002'");
-    console.log(ans)
     res.render("index")
 });
 
